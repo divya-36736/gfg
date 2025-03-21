0ff95b547fe15d5f0da2ed89ff48da40edbfa790
divya-36736 <23bec036@nith.ac.in>
2025-03-21 18:28:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Please change the array in-place
+    void insertionSort(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        for(int  i = 1; i<n; i++){
+            int key =  arr[i];
+            int j;
+            for(j = i-1 ; j>=0 && arr[j] > key; j--){
+                
+                arr[j+1] = arr[j];
+                
+            }
+            arr[j+1] = key;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        obj.insertionSort(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
