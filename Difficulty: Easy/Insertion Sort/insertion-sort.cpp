66d8257dea50215fa35231789640d050c53938a9
divya-36736 <23bec036@nith.ac.in>
2025-03-23 08:10:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -7,20 +7,28 @@ using namespace std;
 
 class Solution {
   public:
-    // Please change the array in-place
+    void insertionSortRecursive(vector<int> &arr, int n){
+        if(n <= 1) return;
+        
+        insertionSortRecursive(arr, n-1);
+        int last = arr[n - 1];
+        
+        int j = n - 2;
+        
+        while(j>=0 && arr[j] > last){
+            arr[j+1] = arr[j];
+            j--;
+        }
+    
+        arr[j+1] = last;
+    
+    }
+    
     void insertionSort(vector<int>& arr) {
         // code here
-        int n = arr.size();
-        for(int  i = 1; i<n; i++){
-            int key =  arr[i];
-            int j;
-            for(j = i-1 ; j>=0 && arr[j] > key; j--){
-                
-                arr[j+1] = arr[j];
-                
-            }
-            arr[j+1] = key;
-        }
+        
+        insertionSortRecursive(arr, arr.size());
+    
     }
 };
 
